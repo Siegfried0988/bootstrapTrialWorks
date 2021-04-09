f9c7341f83b6cf0305e617c9eec72dd695f76672
Tony Chiu <tw_tony_chiu@dg.carrefour.com>
2021-04-09 14:04:21 +0800

    add two page

--- js/all.js
+++ js/all.js
@@ -1,44 +1,6 @@
 $(document).ready(function () {
-  $.ajax({
-    method: "GET",
-    // url: "https://randomuser.me/api/",
-    url: "https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=6",
-    dataType: 'json',
-    data: '',
-    async: true,
-    // data: "data",
-    // dataType: "dataType",
-    // success: function (response) {
-    // }
-  })
-    .done(function (msg) {
-      // console.log('全部: ' + msg)
-      // console.log(msg)
-      // console.log(msg.results[0].email)
-      // console.log('信箱: ' + msg.results[0].email)
-      // $('.font1x').append(msg.results[i].email);
+  
+ 
 
-
-
-
-
-      for (let index = 0; index < 3; index++) {
-        $('.newTbody').append('<tr> <th scope="row">' + (index+4) + '</th><th scope="col">' + msg[index].endDate + '</th> <th scope="col">' + msg[index].sourceWebName + '</th> <th scope="col">' + msg[index].title +'</th></tr>');
-
-      }
-      console.log(msg[0])
-
-      $("#myInput").on("keyup", function() {
-        var value = $(this).val().toLowerCase();
-        $("#myTable tr").filter(function() {
-          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-        });
-      });
-      
-
-      
-
-    })
-    $('#table').bootstrapTable()
+  $('#table').attr('data-url', './taipeiPark.json');
 });
-
---
+++ script.js
@@ -0,0 +1,11 @@
+document.write('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous">');
+document.write('<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" crossorigin="anonymous">');
+document.write('<link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.15.5/dist/bootstrap-table.min.css">');
+document.write('<script src="https://code.jquery.com/jquery-3.3.1.min.js" crossorigin="anonymous"></script>');
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" crossorigin="anonymous"></script>');
+document.write('<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" crossorigin="anonymous"></script>');
+document.write('<script src="https://unpkg.com/bootstrap-table@1.18.2/dist/bootstrap-table.min.js"></script>');
+document.write('<link rel=stylesheet type="text/css" href="./css/all.css">');
+document.write('<script src="js/all.js"></script>');
+
+
